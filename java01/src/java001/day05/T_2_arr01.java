f9c4c16b58d67ec98909c68850e2a19e896ca5fb
梁浩天 <l15304704058@126.com>
2025-09-23 21:58:34 +0800

    feat: 新增二维数组初始化示例代码

---
+++ java01/src/java001/day05/T_2_arr01.java
@@ -0,0 +1,23 @@
+package java001.day05;
+import java.util.Scanner;
+
+//二维数组的初始化
+
+public class T_2_arr01 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int[][] a = new int[3][3];
+        for (int i = 0; i < 3; i++) {
+            for(int j = 0;j<3;j++){
+                int num = scan.nextInt();
+                a[i][j] = num;
+            }
+        }
+        for(int i = 0;i<3;i++){
+            for(int j = 0;j<3;j++){
+                System.out.print(a[i][j] + "  ");
+            }
+            System.out.println();
+        }
+    }
+}
