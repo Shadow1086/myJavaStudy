83d783c6b72813bec2492c2d98ff4c0ae58b8ff7
梁浩天 <l15304704058@126.com>
2025-09-23 20:11:00 +0800

    while循环

---
+++ java01/src/java001/day02/T_while02.java
@@ -0,0 +1,13 @@
+package java001.day02;
+
+public class T_while02 {
+    public static void main(String [] args){
+        int num = 6;
+        int sum = 0;
+        while(num <= 180){
+            sum += num;
+            num += 6;
+        }
+        System.out.println("6 + 12 + 18 +...+ 180 = "+sum);
+    }
+}
---
+++ java01/src/java001/day02/T_while04.java
@@ -0,0 +1,18 @@
+package java001.day02;
+
+public class T_while04 {
+    public static void main(String[] args){
+//        小明和小红玩报数游戏，小明按1〜20报数，小红按1〜30报数。
+//        若两人同时开始，并以同样的速度报数，当两人都报了 1000个数时，同时报相同数的次数是多少？
+        int numHong = 1,numMing = 1 ,sum = 1,time = 0;
+        while(sum<=1000){
+            if(numHong > 20)        numHong = 1;
+            if(numMing > 30)        numMing = 1;
+            if(numHong == numMing)      time += 1;
+            numHong ++;
+            numMing ++;
+            sum ++;
+        }
+        System.out.println("小明与小红同时报相同数的次数是"+time+"次。");
+    }
+}
