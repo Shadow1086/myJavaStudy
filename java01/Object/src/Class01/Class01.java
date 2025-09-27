2e2fcb96411364d9be690f67ae24780893bdb60d
梁浩天 <l15304704058@126.com>
2025-09-27 20:31:44 +0800

    feat: 新增Class01类

---
+++ java01/Object/src/Class01/Class01.java
@@ -0,0 +1,16 @@
+package Class01;
+
+/**
+ * ClassName: Practice01
+ * Package: java001.day05
+ * Description:
+ * {@code @Author} Liang-ht
+ * {@code @Create} 2025/9/27 14:19
+ * {@code @Version} 1.0
+ */
+
+public class Class01 {
+    public static void main(String[] args) {
+
+    }
+}
