79a6aa6179a53524d95032ce4211d7670c108087
梁浩天 <l15304704058@126.com>
2025-09-28 14:04:17 +0800

    feat: 新增封装性案例02

---
+++ java01/Object/src/Class01/encapsulation01/PersonTest.java
@@ -0,0 +1,40 @@
+package Class01.encapsulation01;
+
+/**
+ * ClassName: Practice01
+ * Package: java001.day05
+ * Description: 定义两个类，Person和PersonTest，用setAge设置人的合法年龄，用getAge返回人的年龄。
+ * 在PersonTest类中实例化Person类的对象，调用setAge和getAge方法，体会java的封装性。
+ * {@code @Author} Liang-ht
+ * {@code @Create} 2025/9/28 13:55
+ * {@code @Version} 1.0
+ */
+
+public class PersonTest {
+    public static void main(String[] args) {
+        Person p1 = new Person();
+        p1.setAge(46);
+        System.out.println("设置的年龄为：" + p1.getAge());
+    }
+
+}
+
+class Person {
+    //属性
+    private int age;
+
+    //方法
+    //设置
+    public void setAge(int num) {
+        if (num > 0 && num < 120) {
+            age = num;
+        } else {
+            System.out.println("输入数据有误");
+        }
+    }
+
+    //读取
+    public int getAge() {
+        return age;
+    }
+}
