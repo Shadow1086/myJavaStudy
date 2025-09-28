0ccd236dacde4de9186d50380b20d6a7706da5e7
梁浩天 <l15304704058@126.com>
2025-09-28 16:16:29 +0800

    .

---
+++ java01/Object/src/Class01/Encapsulation04/Employee.java
@@ -0,0 +1,61 @@
+package Class01.Encapsulation04;
+
+/**
+ * ClassName: Practice01
+ * Package: java001.day05
+ * Description:声明普通员工类Employee，提供get/set方法，并提供String getInfo()方法
+ * 在测试类的main中创建员工数组，并从键盘输入员工对象信息，最后遍历输出
+ * {@code @Author} Liang-ht
+ * {@code @Create} 2025/9/28 14:26
+ * {@code @Version} 1.0
+ */
+
+public class Employee {
+    //属性
+    private String name;
+    private char sex;
+    private int age;
+    private String teleNum;
+
+    //方法
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
+
+    public void setAge(int age){
+        if(age>0&&age<120){
+            this.age= age;
+        }else{
+            System.out.println("输入年龄有误");
+        }
+    }
+    public int  getAge(){
+        return age;
+    }
+
+    public void setSex(char sex){
+        if(sex =='男'||sex == '女'){
+            this.sex = sex;
+        }else{
+            System.out.println("输入性别有误");
+        }
+    }
+    public char getSex(){
+        return sex;
+    }
+
+    public void setTeleNum(String num){
+        this.teleNum = num;
+    }
+    public String getTeleNum(){
+        return teleNum;
+    }
+
+    //显示总信息方法
+    public String getInfo(){
+        return (name + "\t" + sex + "\t" + age + "\t" + teleNum);
+    }
+}
---
+++ java01/Object/src/Class01/Encapsulation04/EmployeeTest.java
@@ -0,0 +1,45 @@
+package Class01.Encapsulation04;
+
+import java.util.Scanner;
+
+/**
+ * ClassName: Practice01
+ * Package: java001.day05
+ * Description:
+ * {@code @Author} Liang-ht
+ * {@code @Create} 2025/9/28 14:36
+ * {@code @Version} 1.0
+ */
+
+public class EmployeeTest {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("请输入员工总数：");
+        int num = input.nextInt();
+
+        Employee[] arrEm = new Employee[num];
+//给数组中的各个成员进行赋值
+        for (int i = 0; i < arrEm.length; i++) {
+            arrEm[i] = new Employee();
+            System.out.println("--------添加第" + (i + 1) + "个员工----------");
+            System.out.print("姓名：");
+            arrEm[i].setName(input.next());
+
+            System.out.print("性别：");
+            arrEm[i].setSex(input.next().charAt(0));
+
+            System.out.print("年龄：");
+            arrEm[i].setAge(input.nextInt());
+
+            System.out.print("电话：");
+            arrEm[i].setTeleNum(input.next());
+        }
+        //遍历显示信息
+        System.out.println("----------员工列表----------");
+        System.out.println("编号\t姓名\t性别\t年龄\t电话");
+        for(int i = 0;i<arrEm.length;i++){
+            System.out.println((i+1) + "\t" + arrEm[i].getInfo());
+        }
+        System.out.println("----------员工列表完成----------");
+    }
+}
