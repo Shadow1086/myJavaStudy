6aaa4c4a37ffe0c5aa43e7b00c1687491d27baf4
梁浩天 <l15304704058@126.com>
2025-10-02 22:54:33 +0800

    feat(kids): add Kids and ManKind classes with age and employment methods

---
+++ java01/Object/src/Class03/Super01/Kids.java
@@ -0,0 +1,45 @@
+package Class03.Super01;
+
+import Class03.Extend01.Mankind;
+
+/**
+ * ClassName: Practice01
+ * Package: Class03.Extend01.Super
+ * Description:
+ * {@code @Author} Liang-ht
+ * {@code @Create} 2025/10/2 21:49
+ * {@code @Version} 1.0
+ */
+
+public class Kids extends Mankind {
+    private int yearsOld;
+
+    public Kids(){}
+
+    public Kids(int sex, int salary, int yearsOld) {
+        super(sex, salary);
+        this.yearsOld = yearsOld;
+    }
+
+    public Kids(int yearsOld) {
+        this.yearsOld = yearsOld;
+    }
+
+    public int getYearsOld() {
+        return yearsOld;
+    }
+
+    public void setYearsOld(int yearsOld) {
+        this.yearsOld = yearsOld;
+    }
+
+    public void printAge(){
+        System.out.println(yearsOld);
+    }
+
+    public void employeed(){
+        super.employeed();
+        System.out.println("but Kids should study and no job");
+    }
+
+}
