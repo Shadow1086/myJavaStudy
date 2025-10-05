a1ffdd1043a701327f0bb6b3785e2d59cf5d7e31
梁浩天 <l15304704058@126.com>
2025-10-05 17:44:56 +0800

    feat: 添加Circle和GeometricObject类及其方法,对于equals以及toString方法的重写

---
+++ java01/Object/src/Class05/Object/ToString/Exer01/GeometricObject.java
@@ -0,0 +1,41 @@
+package Class05.Object.ToString.Exer01;
+
+/**
+ * ClassName: GeometricObject
+ * Package: Class05.Object.ToString.Exer01
+ * Description:
+ * {@code @Author} Liang-ht
+ * {@code @Create} 2025/10/5 17:33
+ * {@code @Version} 1.0
+ */
+
+public class GeometricObject {
+    protected String color;
+    protected double weight;
+
+    public GeometricObject() {
+        this.color = "White";
+        this.weight = 1.0;
+    }
+
+    public GeometricObject(String color, double weight) {
+        this.color = "White";
+        this.weight = 1.0;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+}
