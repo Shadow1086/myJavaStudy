da5f2a92aa690ae1461171509778aa3996453899
梁浩天 <l15304704058@126.com>
2025-10-05 10:25:21 +0800

    feat: 添加Man和Woman类，演示多态性及特有方法调用

---
+++ java01/Object/src/Class04/Exer06/Person.java
@@ -0,0 +1,21 @@
+package Class04.Exer06;
+
+/**
+ * ClassName: Person
+ * Package: Class04.Exer06
+ * Description:
+ * {@code @Author} Liang-ht
+ * {@code @Create} 2025/10/5 10:03
+ * {@code @Version} 1.0
+ */
+
+public class Person {
+    public void eat(){
+        System.out.println("人吃饭");
+    }
+
+    public void toilet(){
+        System.out.println("人去洗手间");
+    }
+    
+}
