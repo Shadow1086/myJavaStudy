d8b484652629f04dcaad914299ccd723b000d0bc
linux <l15304704058@126.com>
2025-10-10 21:29:07 +0800

    feat:

---
+++ java01/Object/src/Class07/SingleTon/Abstract/Apply01/Person.java
@@ -0,0 +1,13 @@
+package Class07.SingleTon.Abstract.Apply01;
+
+
+/**
+ * ClassName: Person
+ * Package: Class07.SingleTon.Abstract.Apply01
+ * Description:
+ * {@code @Author} Liang-ht
+ * {@code @Create} 2025/10/10 21:28
+ * {@code @Version} 1.0
+ */
+public class Person {
+}
---
+++ java01/Object/src/Class07/SingleTon/Abstract/Apply02/Test.java
@@ -0,0 +1,13 @@
+package Class07.SingleTon.Abstract.Apply02;
+
+
+/**
+ * ClassName: Test
+ * Package: Class07.SingleTon.Abstract.Apply02
+ * Description:
+ * {@code @Author} Liang-ht
+ * {@code @Create} 2025/10/10 21:28
+ * {@code @Version} 1.0
+ */
+public class Test {
+}
