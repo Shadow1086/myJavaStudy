39d6185359eb46a7aeece25fa6e0352cbe25ee19
梁浩天 <l15304704058@126.com>
2025-10-11 20:02:57 +0800

    feat: 添加OuterClassTest类及内部类示例，增强内部类的理解和使用说明

---
+++ java01/Object/src/Class09/inner/Apply01/OuterClassTest.java
@@ -0,0 +1,32 @@
+package Class09.inner.Apply01;
+
+/**
+ * ClassName: OuterClassTest
+ * Package: Class09.inner.Apply01
+ * Description:
+ * {@code @Author} Liang-ht
+ * {@code @Create} 2025/10/11 17:43
+ * {@code @Version} 1.0
+ */
+
+public class OuterClassTest {
+
+
+}
+
+
+class Person{
+    public void method(){
+
+    }
+
+    //内部类
+    static class Dog{
+
+    }
+
+    //非静态的成员内部类
+    class Bird{
+
+    }
+}
