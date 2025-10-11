004e0a60ac5c1188e89d958d9bf57d97a39fa303
梁浩天 <l15304704058@126.com>
2025-10-11 21:55:53 +0800

    feat: 添加OuterClassTest和OuthClassTest01类，演示内部类的使用及局部内部类的示例

--- java01/Object/src/Class09/inner/Apply01/OuterClassTest.java
+++ java01/Object/src/Class09/inner/Apply01/OuterClassTest.java
@@ -10,23 +10,61 @@ package Class09.inner.Apply01;
  */
 
 public class OuterClassTest {
+    public static void main(String[] args) {
+        //1.创建Person的静态的成员内部类的实例
+        Person.Dog dog = new Person.Dog();
+        dog.eat();
 
-
+        //2.创建Person的非静态的成员内部类的实例
+        Person p1 = new Person();
+        Person.Bird bird = p1.new Bird();
+        bird.eat();
+        bird.show("atm");
+        bird.show1();
+    }
 }
 
 
 class Person{
-    public void method(){
+    String name = "Tom";
+    int age = 1;
 
-    }
 
-    //内部类
+    //静态的成员内部类
     static class Dog{
-
+        public void eat(){
+            System.out.println("dog 吃骨头");
+        }
     }
 
     //非静态的成员内部类
     class Bird{
+        String name = "bird";
+        public void eat(){
+            System.out.println("鸟吃鸟粮");
+        }
+        public void show(String name){
+            System.out.println("age = " + age);
+            System.out.println("name = " + name);
+            System.out.println("this.name = " + this.name);
+            System.out.println("Person.this.name = " + Person.this.name);
 
+        }
+        public void show1(){
+            eat();
+            this.eat();
+            Person.this.eat();
+        }
+    }
+
+    public void eat(){
+        System.out.println("人吃饭");
+    }
+
+       public void method(){
+        //局部内部类
+        class InnerClass1{
+
+        }
     }
 }
---
+++ java01/Object/src/Class09/inner/Exer01/ObjectTest.java
@@ -0,0 +1,19 @@
+package Class09.inner.Exer01;
+
+public class ObjectTest{
+    public static void main(String[] args) {
+        //提供一个继承于Object的匿名子类
+        new Object(){
+            public void eat(){
+                
+            }
+        }
+    }
+}
+
+
+
+
+class Object {
+    
+}
