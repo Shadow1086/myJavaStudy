a0fb0859e595272b798814630c40edbd147a66bf
梁浩天 <l15304704058@126.com>
2025-10-11 17:04:31 +0800

    feat: 添加CompareA和CompareB接口及其实现类SubClass，支持接口的静态和默认方法

---
+++ java01/Object/src/Class08/Interface/JDK8/CompareB.java
@@ -0,0 +1,16 @@
+package Class08.Interface.JDK8;
+
+
+/**
+ * ClassName: CompareB
+ * Package: Class08.Interface.JDK8
+ * Description:
+ * {@code @Author} Liang-ht
+ * {@code @Create} 2025/10/11 16:53
+ * {@code @Version} 1.0
+ */
+public interface CompareB {
+    public default void method3(){
+        System.out.println("compareB 静态方法2的调用");
+    }
+}
---
+++ java01/Object/src/Class08/Interface/JDK8/SubClass.java
@@ -0,0 +1,18 @@
+package Class08.Interface.JDK8;
+
+/**
+ * ClassName: SubClass
+ * Package: Class08.Interface.JDK8
+ * Description:
+ * {@code @Author} Liang-ht
+ * {@code @Create} 2025/10/11 16:53
+ * {@code @Version} 1.0
+ */
+
+public class SubClass implements CompareA{
+    @Override
+    public void method2(){
+        System.out.println("SubClass中method2的重写");
+    }
+
+}
