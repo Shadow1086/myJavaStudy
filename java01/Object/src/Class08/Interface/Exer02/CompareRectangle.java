276cefd3496e04ce8470fe462c07f25495432cdd
梁浩天 <l15304704058@126.com>
2025-10-11 16:22:02 +0800

    feat: 添加Circle、Rectangle及其比较实现类，支持对象比较功能

---
+++ java01/Object/src/Class08/Interface/Exer02/CompareRectangle.java
@@ -0,0 +1,27 @@
+package Class08.Interface.Exer02;
+
+/**
+ * ClassName: CompareRectangle
+ * Package: Class08.Interface.Exer02
+ * Description:
+ * {@code @Author} Liang-ht
+ * {@code @Create} 2025/10/11 16:14
+ * {@code @Version} 1.0
+ */
+
+public class CompareRectangle extends Rectangle implements CompareObject{
+    public CompareRectangle(double length, double weight) {
+        super(length, weight);
+    }
+
+    @Override
+    public int compareTo(Object object){
+        if(object instanceof Rectangle rectangle){
+                if(rectangle.findArea()>this.findArea()) return -1;
+                else if (rectangle.findArea()<this.findArea())   return 1;
+                else if(rectangle.findArea()==this.findArea())   return 0;
+        }
+        return -100;
+    }
+
+}
