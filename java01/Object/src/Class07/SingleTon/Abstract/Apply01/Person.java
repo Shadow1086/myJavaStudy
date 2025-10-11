4ddb7f9d6b207e1ac6b7a01696efe8bbeb5257db
梁浩天 <l15304704058@126.com>
2025-10-11 10:25:28 +0800

    feat: 将Person类改为抽象类并添加Student类，定义抽象方法以实现多态

--- java01/Object/src/Class07/SingleTon/Abstract/Apply01/Person.java
+++ java01/Object/src/Class07/SingleTon/Abstract/Apply01/Person.java
@@ -9,5 +9,19 @@ package Class07.SingleTon.Abstract.Apply01;
  * {@code @Create} 2025/10/10 21:28
  * {@code @Version} 1.0
  */
-public class Person {
+public abstract class Person {
+    String name;
+    int age;
+
+    public Person(){};
+
+    public Person(String name ,int age){
+        this.name  = name;
+        this.age = age;
+    }
+
+    //两个抽象方法
+    public abstract void eat();
+
+    public abstract void sleep();
 }
