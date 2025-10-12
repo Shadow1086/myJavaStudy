6f64921213a08274bf105b47c1a642fb24b0e8e1
梁浩天 <l15304704058@126.com>
2025-10-12 22:04:14 +0800

    feat: 新增Employee类、Status枚举及其测试类，定义员工信息和状态

---
+++ java01/Object/src/Class10/_enum/Apply03/Status.java
@@ -0,0 +1,16 @@
+package Class10._enum.Apply03;
+/**
+ * ClassName: Status
+ * Description:
+ * 
+ * 定义公司员工的状态
+ * 
+ * {@code @Author} Liang-ht
+ * {@code @Create} 2025-10-12 21:46:39
+ * {@code @Version} 1.0
+ */
+public enum Status {
+    
+    BUSY, FREE, VOCATION, DIMISSION;
+
+}
