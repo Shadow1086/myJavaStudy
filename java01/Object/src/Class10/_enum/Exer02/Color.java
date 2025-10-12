b9b084651cbae0651f7403778d4a11e34eb0aa04
梁浩天 <l15304704058@126.com>
2025-10-12 22:32:46 +0800

    feat: 新增Color枚举类及其测试类，定义颜色及其RGB值和描述

---
+++ java01/Object/src/Class10/_enum/Exer02/Color.java
@@ -0,0 +1,31 @@
+package Class10._enum.Exer02;
+
+public enum Color {
+    RED(255,0,0,"红色"), 
+    ORANGE(255,128,0,"橙色"), 
+    YELLOW(255,255,0,"黄色"), 
+    GREEN(0,255,0,"绿色"), 
+    CYAN(0,255,255,"青色"),
+    BLUE(0,0,255,"蓝色"),
+    PURPLE(128,0,255,"紫色");
+
+
+    private final int red;
+    private final int green;
+    private final int blue;
+
+    private final String description;
+
+    private Color(int red, int green, int blue,String description) {
+        this.red = red;
+        this.green = green;
+        this.blue = blue;
+        this.description = description;
+    }
+
+    @Override
+    public String toString(){
+        return super.name() + "(" + red + "," + green + "," + blue + ")" + "----->" + description;
+    }
+
+}
---
+++ java01/Object/src/Class10/_enum/Exer02/ColorTest.java
@@ -0,0 +1,8 @@
+package Class10._enum.Exer02;
+
+public class ColorTest {
+    public static void main(String[] args) {
+        System.out.println(Color.BLUE.toString());
+        System.out.println(Color.GREEN.toString());
+    }
+}
