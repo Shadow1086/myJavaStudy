a83561ef49353f1d24763c1130b11acc1d06c4b7
Liang-ht <l15304704058@126.com>
2025-11-05 21:28:35 +0800

    ✨ feat: 新增String类相关示例
    - 添加了StringDemo01类，展示String的基本用法及不可变性
    - 新增StringDemo2类，包含String类的实例化与连接操作
    - 每个类中均包含JUnit测试方法，便于验证功能

---
+++ java02/src/Class03/API/String01/First/StringDemo2.java
@@ -0,0 +1,16 @@
+package Class03.API.String01.First;
+import org.junit.Test;
+/**
+ * ClassName: StringDemo2
+ * Description: 
+ * 
+ * String类的实例化与连接操作
+ * {@code @Author} Liang-ht
+ * {@code @Create} 2025-11-05 21:03:19
+ */
+public class StringDemo2 {
+    @Test
+    public void test1(){
+        
+    }
+}
