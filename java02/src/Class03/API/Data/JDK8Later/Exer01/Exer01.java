d43c1821b85493bfbb21eab2d4fde33abf0d65dd
Liang-ht <l15304704058@126.com>
2025-11-09 15:42:26 +0800

    ✨ feat: 新增Exer01类及日期计算测试
    - 在java02/src/Class03/API/Data/JDK8Later/目录中新增Exer01.java文件
    - 实现使用Calendar和LocalDateTime获取当前时间及计算出生后100天的日期
    - 包含一个测试方法，展示如何使用Calendar进行日期操作
    - 增强了日期时间相关功能的单元测试覆盖率

---
+++ java02/src/Class03/API/Data/JDK8Later/Exer01/Exer01.java
@@ -0,0 +1,25 @@
+package Class03.API.Data.JDK8Later.Exer01;
+import org.junit.Test;
+import java.util.Calendar;
+
+/**
+ * ClassName: Exer01
+ * Description: 
+ * 
+ * 使用Calendar获取当前时间, 把这个时间设置为你的生日, 再获取你的百天(出生后100天)日期。
+ * 使用LocalDateTime获取当前时间, 把这个时间设置为你的生日, 再获取你的百天(出生后100天)日期。
+ * {@code @Author} Liang-ht
+ * {@code @Create} 2025-11-08 21:48:44
+ */
+public class Exer01 {
+    @Test
+    public void test01(){
+        Calendar calendar  = Calendar.getInstance();
+        System.out.println("你的生日为：" + calendar.getTime());
+
+        calendar.add(Calendar.DAY_OF_YEAR,100);
+        System.out.println(calendar.getTime());
+    }
+
+    
+}
