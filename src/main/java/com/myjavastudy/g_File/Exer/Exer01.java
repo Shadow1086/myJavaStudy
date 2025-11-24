1d55847ea4050407509dd6eff383ea360e857d42
Liang haotian <l15304704058@126.com>
2025-11-24 22:06:11 +0800

    feat: 添加文件创建功能和完善注释说明
    - 将文件路径改为绝对路径以确保文件定位准确
    - 添加 createNewFile() 方法调用实现真正的文件创建
    - 完善代码注释，说明 File 构造器不会在磁盘创建文件的特性
    - 添加文件创建结果的判断和提示信息

--- src/main/java/com/myjavastudy/g_File/Exer/Exer01.java
+++ src/main/java/com/myjavastudy/g_File/Exer/Exer01.java
@@ -12,12 +12,22 @@ import java.io.IOException;
  */
 public class Exer01{
     public static void main(String[] args) throws IOException {
-        File file = new File("hello.txt");
+        File file = new File("/Volumes/study/02-java/myJavaStudy/src/main/java/com/myjavastudy/g_File/Exer/hello.txt");
         System.out.println(file.getAbsolutePath());
 
-        //构造器
-        File file2 = new File(file.getAbsoluteFile().getParent(),"abc.txt");
+        // //构造器
+        // //使用这种方法并不会在磁盘上真正的创建一个文件
+        File file2 = new File(file.getAbsoluteFile().getParent(), "abc.txt");
         System.out.println(file2.getAbsolutePath());
+
+        //如果要真正创建文件，需要调用createNewFile()方法
+        //file2.createNewFile();
+        boolean isFlag = file2.createNewFile();
+        if(isFlag){
+            System.out.println("创建成功");
+        }else {
+            System.out.println("创建失败");
+        }
 
     }
 }
