567ff7a2f7b2f1fc9214d33e917c4e17487dff4b
Liang haotian <l15304704058@126.com>
2025-11-27 18:00:54 +0800

    ✨ feat: 新增 practice02 类以扩展文件操作示例
    - 创建 practice02.java 文件，作为文件操作的练习示例
    - 该类目前为空，为后续功能实现做准备

---
+++ src/main/java/com/myjavastudy/g_File/Exer03/practice02.java
@@ -0,0 +1,34 @@
+package com.myjavastudy.g_File.Exer03;
+
+import java.io.File;
+
+/**
+ * ClassName: practice02
+ * Description:
+ * 
+ * 拓展1：删除指定文件目录及其下的所有文件
+ * public void deleteDirectory(File file) //file可能是文件，也可能是文件目录
+ * {@code @Author} Liang-ht
+ * {@code @Create} 2025-11-27 17:02:31
+ */
+public class practice02 {
+    public static void main(String[] args) {
+        File file = new File("#");
+        deleteDirectory(file);
+    }
+
+    public static void deleteDirectory(File file) {
+        if (!file.exists()) {
+            return;
+        }else if(file.isFile()) {
+            file.delete();
+        } else {
+            File[] fileList = file.listFiles();
+            for (File f : fileList) {
+                deleteDirectory(f);
+            }
+            file.delete();
+        }
+
+    }
+}
