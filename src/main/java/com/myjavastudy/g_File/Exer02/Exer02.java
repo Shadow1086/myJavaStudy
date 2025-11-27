00515dd36e56b25efbd2a701e3cb32738dd65163
Liang haotian <l15304704058@126.com>
2025-11-27 16:46:43 +0800

    ✨ feat: 实现文件过滤功能以列出特定格式的文件
    - 新增 FilenameFilter 以过滤并列出以 jpg 和 png 格式结尾的文件
    - 更新代码示例，展示两种文件列出方式，提升用户对文件操作的理解

--- src/main/java/com/myjavastudy/g_File/Exer02/Exer02.java
+++ src/main/java/com/myjavastudy/g_File/Exer02/Exer02.java
@@ -1,6 +1,7 @@
 package com.myjavastudy.g_File.Exer02;
 
 import java.io.File;
+import java.io.FilenameFilter;
 
 /**
  * ClassName: Exer02
@@ -17,10 +18,26 @@ import java.io.File;
  */
 public class Exer02 {
     public static void main(String[] args) {
-        File file = new File("/Volumes/study/02-java/myJavaStudy/src/main/java/com/myjavastudy/g_File");
-        File[] fileList = file.listFiles();
-        for(File fileEach : fileList){
-            
+        // 方式一：
+        // File file = new File("/Volumes/Users/htliang/Downloads/图标");
+        // String[] filesList = file.list();
+        // for(String s : filesList){
+        // if(s.endsWith("jpg")){
+        // System.out.println("以jpg格式结尾的文件：" + s);
+        // }else if(s.endsWith("png")){
+        // System.out.println("以png格式结尾的文件：" + s);
+        // }
+        // }
+        // 方式二：
+        File dir = new File("/Volumes/Users/htliang/Downloads/图标");
+        String[] fileList = dir.list(new FilenameFilter() {
+            @Override
+            public boolean accept(File dir, String name) {
+                return name.endsWith("png")||name.endsWith("jpg");
+            }
+        });
+        for(String s : fileList){
+            System.out.println(s);
         }
     }
 }
