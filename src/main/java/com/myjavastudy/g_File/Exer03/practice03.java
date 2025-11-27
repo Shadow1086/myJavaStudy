93ca7c93bf6958d8dbb9d5244cb6e2ce66a604e2
Liang haotian <l15304704058@126.com>
2025-11-27 20:17:31 +0800

    ✨ feat: 新增 practice03 类以准备后续文件操作功能
    - 创建 practice03.java 文件，作为文件操作的练习示例
    - 该类目前为空，为后续功能实现做准备

---
+++ src/main/java/com/myjavastudy/g_File/Exer03/practice03.java
@@ -0,0 +1,31 @@
+package com.myjavastudy.g_File.Exer03;
+
+import java.io.File;
+
+/**
+ * ClassName: practice03
+ * Description:
+ * 
+ * 拓展2：计算指定文件目录占用空间的大小
+ * public long getDirectorySize(File file) //file可能是文件，也可能是文件目录
+ * {@code @Author} Liang-ht
+ * {@code @Create} 2025-11-27 18:01:50
+ */
+public class practice03 {
+    public static void main(String[] args) {
+        File file = new File(".");
+        System.out.println(getDirectorySize(file));
+    }
+    public static long getDirectorySize(File file){
+        long size = 0;
+        if(file.isFile()){
+            size = file.length();
+        }else if(file.isDirectory()){
+            File[] fileList = file.listFiles();
+            for(File f : fileList){
+                size += getDirectorySize(f);
+            } 
+        }
+        return size;
+    }
+}
