280162ac37998ac252d4c94e8ce08cc33b862f0b
Liang haotian <l15304704058@126.com>
2025-11-29 16:43:02 +0800

    ✨ feat: 增加使用缓冲流复制图片的功能
    - 在 FileStreamTest.java 中新增 test02 方法，演示如何使用 BufferedInputStream 和 BufferedOutputStream 复制图片文件
    - 添加 fuzhiCopyStream.png 文件作为复制结果
    - 优化资源管理，确保流在操作后正确关闭

--- src/main/java/com/myjavastudy/g_File/FileStream/FileStreamTest/FileStreamTest.java
+++ src/main/java/com/myjavastudy/g_File/FileStream/FileStreamTest/FileStreamTest.java
@@ -1,5 +1,7 @@
 package com.myjavastudy.g_File.FileStream.FileStreamTest;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -9,27 +11,29 @@ import java.io.IOException;
  * ClassName: FileStreamTest
  * Description:
  * 
- * 复制图片等字节文件
+ * 使用字节流复制图片等字节文件
  * 
  * {@code @Author} Liang-ht
  * {@code @Create} 2025-11-28 19:59:34
  */
 public class FileStreamTest {
     public static void main(String[] args) {
-        test01();
+        test02();
+        
     }
 
     /**
      * 需求：复制一份fuzhi.png图片，命名为fuzhiCopy.png
      */
     public static void test01() {
+
         FileInputStream fis = null;
         FileOutputStream fos = null;
         try {
             // 创建File对象
             File srcFile = new File("src/main/resources/fuzhi-L.png");
             File targetFile = new File("src/main/java/com/myjavastudy/g_File/FileStream/FileStreamTest/fuzhiCopy.png");
-            //创建Stream流
+            // 创建Stream流
             fis = new FileInputStream(srcFile);
             fos = new FileOutputStream(targetFile);
 
@@ -43,11 +47,16 @@ public class FileStreamTest {
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
-            //关闭资源
+            // 关闭资源
             try {
                 if (fis != null) {
                     fis.close();
                 }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            try {
                 if (fos != null) {
                     fos.close();
                 }
@@ -55,6 +64,52 @@ public class FileStreamTest {
                 e.printStackTrace();
             }
         }
-        // 1.创建对象
+    }
+
+    /**
+     * 需求：使用BufferedInputStream / BufferedOutputStream 复制一张图片
+     * 
+     * 关键点：缓冲流是处理流，必须包装在节点流（如FileInputStream）之上
+     */
+    public static void test02() {
+        BufferedInputStream bis = null;
+        BufferedOutputStream bos = null;
+
+        try {
+            // 1. 创建File对象
+            File srcFile = new File("src/main/resources/fuzhi-L.png");
+            File targetFile = new File(
+                    "src/main/java/com/myjavastudy/g_File/FileStream/FileStreamTest/fuzhiCopyStream.png");
+            FileInputStream fis = new FileInputStream(srcFile);
+            FileOutputStream fos = new FileOutputStream(targetFile);
+
+            bis = new BufferedInputStream(fis);
+            bos = new BufferedOutputStream(fos);
+
+            byte[] buffer = new byte[1024];
+            int len;
+            while((len = bis.read(buffer))!= -1){
+                bos.write(buffer);
+            }
+            System.out.println("成功");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            // 5. 关闭资源（关闭外层流会自动关闭内层流）
+            try {
+                if (bis != null) {
+                    bis.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();// TODO: handle exception
+            }
+            try {
+                if (bos != null) {
+                    bos.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();// TODO: handle exception
+            }
+        }
     }
 }
